6741fd40c9d475aa1919754ab84000a12cdf73b9
Ilamparithi-I <ilam.bissman@gmail.com>
2024-04-19 11:40:29 -0700

    cic-llm-frontend

---
+++ src/components/card/CourseCard.tsx
@@ -0,0 +1,51 @@
+// react card that takes in a title, image, and description
+import { Button } from '../button/Button';
+import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
+import UploadFileIcon from '@mui/icons-material/UploadFile';
+import GroupAddIcon from '@mui/icons-material/GroupAdd';
+import { Link } from 'react-router-dom';
+
+type CardProps = {
+    courseCode: string;
+    courseSection: string;
+    courseName: string;
+    isInstructor: boolean;
+    courseId: string;
+}
+
+const CourseCard = (props: CardProps) => {
+    return (
+        <div className='rounded-lg shadow-lg w-96 h-52 p-6'>
+            <h1 className='text-xl font-bold'>{props.courseCode} - {props.courseSection}</h1>
+            <p className='text-md font-normal mt-1'>{props.courseName}</p>
+
+            {props.isInstructor ?
+                <div className='flex justify-between mt-6'>
+                    <Link to={`/chat/${props.courseId}`} className='w-1/3 m-1'>
+                        <Button variant='default' className='w-full bg-blue-700'>
+                            <QuestionAnswerIcon />
+                        </Button>
+                    </Link>
+                    <Link to={`/upload/${props.courseId}`} className='w-1/3 m-1'>
+                        <Button variant='default' className='w-full bg-blue-700'>
+                            <UploadFileIcon />
+                        </Button>
+                    </Link>
+                    <Link to={`/users/${props.courseId}`} className='w-1/3 m-1'>
+                        <Button variant='default' className='w-full bg-blue-700'>
+                            <GroupAddIcon />
+                        </Button>
+                    </Link>
+                </div>
+                :
+                <Link to={`/chat/${props.courseId}`}>
+                    <Button variant='default' size={"icon"} className='w-full mt-10 bg-blue-700'>
+                        <QuestionAnswerIcon />
+                    </Button>
+                </Link>
+            }
+        </div>
+    );
+};
+
+export default CourseCard;
---
+++ src/pages/upload/UploadFile.tsx
@@ -0,0 +1,141 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { useParams } from "react-router-dom"
+import {
+    Book,
+    Settings,
+    Trash2Icon
+} from "lucide-react"
+
+import { Table, TableBody, TableRow, TableHead, TableHeader, TableCell } from '@/components/table/Table';
+import { ScrollArea } from '@/components/scroll-area/ScrollArea';
+import { Checkbox } from '@/components/checkbox/Checkbox';
+import { Button } from '@/components/button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faUpload } from '@fortawesome/free-solid-svg-icons';
+import SideNav from '@/components/navbar/SideNav';
+
+type CourseData = {
+    id: string;
+    name: string;
+    status: 'Processing' | 'Uploaded';
+    fileType: 'Class Slide' | 'Syllabus' | 'Class Notes' | 'Announcement' | 'Resources';
+}
+
+
+const UploadFile = () => {
+    const { id } = useParams<{ id: string }>()
+    const [files, setFiles] = useState<any[]>([]);
+
+    useEffect(() => {
+        const getFiles = async () => {
+            const response = await fetch(`${import.meta.env.VITE_BACKEND_API_URL}/courses/${id}/documents`, {
+                method: 'GET',
+            });
+
+            const json = await response.json();
+
+            if (response.ok) {
+                setFiles([...files, ...json]);
+            } else {
+                console.error('Failed to fetch files');
+            }
+        }
+
+        getFiles();
+    }, []);
+
+    const inputFile = useRef<HTMLInputElement>(null);
+    const handleUpload = () => {
+        if (inputFile.current) {
+            inputFile.current.click();
+        }
+    };
+
+    const handleFileChange = async (event: any) => {
+        const file = event.target.files[0];
+        if (file) {
+            const formData = new FormData();
+            formData.append('document', file);
+            const response = await fetch(`${import.meta.env.VITE_BACKEND_API_URL}/courses/${id}/documents`, {
+                method: 'POST',
+                body: formData,
+            });
+
+            const json = await response.json();
+            console.log(json);
+
+            if (response.ok) {
+                setFiles([...files, json.file_data]);
+            } else {
+                console.error('Failed to upload document');
+            }
+        }
+    }
+
+    const handleDelete = (documentId: string) => async () => {
+        console.log('Deleting document with id: ' + documentId);
+        const response = await fetch(`${import.meta.env.VITE_BACKEND_API_URL}/courses/${id}/documents/${documentId}`, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            console.log('Document deleted successfully');
+            setFiles(files.filter((file) => file.id !== documentId));
+        } else {
+            console.error('Failed to delete document');
+        }
+    }
+
+    return (
+        <>
+            <div className='flex flex-row' >
+                <SideNav navItems={[
+                    {
+                        url: "/dashboard",
+                        name: "Courses",
+                        icon: <Book size={24} />,
+                    },
+                    // {
+                    //     url: "/settings",
+                    //     name: "Settings",
+                    //     icon: <Settings size={24} />,
+                    // },
+                ]} />
+                <ScrollArea className="h-screen w-full rounded-md border pl-12 pr-12 ">
+                    <div className="flex justify-between items-center mt-2">
+                        <h1 className='text-xl font-bold'>Documents</h1>
+                        <div>
+                            <Button variant={'outline'} className='mr-2' onClick={handleUpload}><FontAwesomeIcon icon={faUpload} className='mr-2' /> Upload</Button>
+                            <input type='file' ref={inputFile} onChange={handleFileChange} className='hidden' />
+                            <Button variant={'destructive'} className='hidden'>Delete</Button>
+                        </div>
+                    </div>
+
+                    <Table>
+                        <TableHeader >
+                            <TableRow>
+                                <TableHead><Checkbox /></TableHead>
+                                <TableHead>Filename</TableHead>
+                                <TableHead></TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        {
+                            files.map((data: CourseData) => (
+                                <TableBody>
+                                    <TableRow>
+                                        <TableCell><Checkbox /></TableCell>
+                                        <TableCell className="font-medium">{data.name}</TableCell>
+                                        <TableCell><Trash2Icon className="text-red-500 cursor-pointer" onClick={handleDelete(data.id)} /></TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            ))
+                        }
+                    </Table>
+                </ScrollArea>
+            </div>
+        </>
+    );
+};
+
+export default UploadFile;
